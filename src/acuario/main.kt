2cfefa85c651ebbefe978352b0b1ca1f9f455b67
DnilsonAH <dnilson.achahuanco@tecsup.edu.pe>
2024-09-17 21:38:42 -0500

    Herencia terminada

---
+++ src/acuario/main.kt
@@ -0,0 +1,24 @@
+package acuario
+
+fun construirAcuario() {
+    val miAcuario = Acuario(ancho = 25, largo = 25, alto = 40)
+    miAcuario.imprimirTamano()
+    val miTorre = TanqueTorre(diametro = 25, alto = 40)
+    miTorre.imprimirTamano()
+
+}
+fun crearPeces() {
+    val tiburon = Tiburon()
+    val pezPayaso = PezPayaso()
+
+    println("Color del tiburón: ${tiburon.color}")
+    tiburon.comer()
+
+    println("Color del pez payaso: ${pezPayaso.color}")
+    pezPayaso.comer()
+}
+fun main() {
+    construirAcuario()
+    crearPeces()
+
+}
